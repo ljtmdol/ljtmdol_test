77cb9ae21cd218df67557eda8e183ff9857d417f
ljtmdol <35207938+ljtmdol@users.noreply.github.com>
2018-01-07 21:27:13 -0600

    Add files via upload

---
+++ x1.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*- 
+#!/usr/bin/python
+import MySQLdb
+from datetime import datetime, timedelta,date
+import time
+
+import calendar
+
+import pdb
+import json
+import binascii
+
+import xlrd
+
+import chardet
+import re
+import pickle
+
+
+import x2
+import x3
+
+x = "aaa,bbb,ccc"
+if "x" in x:
+    print "yyyyy"
+else:
+    print "nnnn"
+
+
+
+
